ce16dd5a60c83759910655ee3e98dcbc1c309b07
Asikur <asikur58039@gmail.com>
2024-02-15 01:48:29 +0600

    first commit

---
+++ admin/admin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+       <!-- navbar -->
+
+       <?php include 'navbar.html'; ?>
+       
+
+        <!-- Sidebar -->
+
+        <?php include 'sidebar.html'; ?>
+
+        <main class="p-4 md:ml-64 h-auto pt-20">
+            Hello World!
+        </main>
+    </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
+</body>
+
+</html>
---
+++ js/custom.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById('logInModal').click();
+  });
+//   document.addEventListener("DOMContentLoaded", function(event) {
+//     document.getElementById('signUpModal').click();
+//   });
